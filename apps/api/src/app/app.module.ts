e663633a3f03e224503682be1c2c09523df2f93c
AnthonyDooley <anthonylouisdooley@gmail.com>
2022-06-18 14:30:09 -0400

    changes for deployment

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,19 +2,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { WordCheckerModule } from './word/word.module';
 import { IdModule } from './id/id.module';
+import { join } from 'path';
+import {ServeStaticModule} from "@nestjs/serve-static";
+
 @Module({
   imports: [WordCheckerModule,
     IdModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host:  process.env.HOST,
       port: 3306,
-      username: 'username',
-      password:'pass',
-      database:'wordle',
-      autoLoadEntities: true,
-      synchronize: true,
-  })
+      username:  process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database:'heroku_00a972e8693a723',
+  }),
+   ServeStaticModule.forRoot({
+        rootPath: join(__dirname, '..', 'wordle'),
+        exclude: ['/api*']
+      }),
 ],
   controllers: [],
   providers: [],
